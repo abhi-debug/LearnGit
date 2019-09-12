43c3ca1c6dbab654d647aed3076a7da130632033
abh-debug <chaurasiaservices@gmail.com>
2019-09-12 12:52:19 +0530

    added DurianFM

---
+++ DurianFM/app/src/main/java/com/example/durianfm/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.durianfm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://m.durianfm.com/");
+        webView.setMinimumHeight(1000);
+        webView.setMinimumWidth(1000);
+        
+    }
+}
